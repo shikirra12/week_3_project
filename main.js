87bc331201e805e95f90b18be969c348a9b85368
Shikirra Williams <ShikirraWilliams@shikirras-mbp.lan>
2017-08-07 08:17:33 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let keys = document.getElementsByTagName('span');
+let display = document.querySelector('.display'
+)
+let value;
+
+for (var i = 0; i < keys.length; i++) {
+  keys[i].onclick = function (action) {
+    
+  }
+  return answer;
+}
