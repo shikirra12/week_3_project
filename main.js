873ff6255089ea78c4e47195403e6a7ade2ab5e3
Shikirra Williams <shikirra.williams@gmail.com>
2017-09-26 16:42:38 -0400

    fixed functionality

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-let keys = document.getElementsByTagName('span');
-let display = document.querySelector('.display'
-)
-let value;
+let keys = document.getElementsByClassName('button');
+let operations = document.getElementsByClassName('operation');
+let evaluate = document.getElementById('evaluate');
+let decimalAdded = false;
+let clearButton = document.querySelector('#clear');
+let display = document.querySelector('#displayValue');
+let storedValue = "";
 
-for (var i = 0; i < keys.length; i++) {
-  keys[i].onclick = function (action) {
-    
-  }
-  return answer;
-}
+  for (var i = 0; i < keys.length; i++) {
+    keys[i].onclick = function makeCalcWork() {
+      let buttonValue = this.innerHTML;
+        display.innerHTML += buttonValue;
+      }
+    }
+
+    clearButton.onclick = function clear() {
+        display.innerHTML = "";
+        storedValue = "";
+    }
+
+    evaluate.onclick = function() {
+      console.log("evaling");
+      storedValue += display.innerHTML;
+      display.innerHTML = eval(storedValue);
+      storedValue = "";
+    }
+
+    for (var i = 0; i < operations.length; i++) {
+      operations[i].onclick = function operators() {
+        storedValue += display.innerHTML;
+        storedValue += this.innerHTML;
+        display.innerHTML = "";
+        console.log(storedValue);
+      }
+    }
